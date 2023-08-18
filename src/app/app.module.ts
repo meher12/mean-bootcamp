6265aa045b15e1307e4e6013ea8ee1611ece11dc
meher12 <meher.khemiri@gmail.com>
2023-08-18 17:44:22 +0100

    Adding Image Controls to Store the Image and Adding an Image Preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 //import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -14,6 +14,7 @@ export class PostCreateComponent implements OnInit {
   updatePost: Post;
   isLoading = false;
   form: FormGroup;
+  imagePreview: any;
   private mode = 'create';
   private postId: string;
   
@@ -23,10 +24,13 @@ export class PostCreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      'title': new FormControl(null, {
+        title: new FormControl(null, {
          validators: [Validators.required, Validators.minLength(3)]
         }),
-        'content': new FormControl(null, {
+        content: new FormControl(null, {
+          validators: [Validators.required]
+         }),
+         image: new FormControl(null, {
           validators: [Validators.required]
          }),
     });
@@ -55,6 +59,17 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+
+  }
 
   // Getting User Input with ngModule
   onSavePost() {
